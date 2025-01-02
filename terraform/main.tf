f42dcf7761abc4f348f73e982dbbe204fa54b433
sudo-gigi <gloryeziani@gmail.com>
2025-01-03 00:14:50 +0100

    comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,20 +77,22 @@ resource "aws_instance" "web_server" {
                       p {
                           font-size: 1.2rem;
                       }
-                      #.witty-text {
-                      #    margin-top: 30px;
-                      #    font-style: italic;
-                      #    font-weight: bold;
-                      #    color: #ffffff;
-                      #    text-shadow: 1px 1px 2px #000000;
-                     # }
+                      <!--
+                      .witty-text {
+                          margin-top: 30px;
+                          font-style: italic;
+                          font-weight: bold;
+                          color: #ffffff;
+                          text-shadow: 1px 1px 2px #000000;
+                      }
+                      <!--
                   </style>
               </head>
               <body>
                   <h1>Hi, I'm Glory Eziani 👩‍💻</h1>
                   <p>Welcome to my landing page powered by Terraform, AWS CodePipeline, and EC2 🚀</p>
                   <p>I'm passionate about Cloud Computing and DevOps 🌥️.</p>
-                  #<p class="witty-text">Detect any changes? Yup, you guessed right! 🚨</p>
+                  <!-- <p class="witty-text">Detect any changes? Yup, you guessed right! 🚨</p> -->
               </body>
               </html>
               HTML
